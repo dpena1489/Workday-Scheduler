f5f6c945089704cf27cb9f94dee0eac631e865ad
dpena1489 <150938385+dpena1489@users.noreply.github.com>
2024-05-04 13:12:49 -0500

    added styling and fixed bugs

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,66 @@
-// Wraped all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-$(document).ready(function () {
-  var currentHour = dayjs().hour();
-  $('#currentDay').text(dayjs().format('MMM DD,YYYY'))
+const localeSettings = {};
+dayjs.locale(localeSettings);
 
+$(function () {
+  //gets current hour from dayjs library
+  const currentHour = dayjs().format('H');
+  //colors change depending on the following: past, present, future.
+  //uses jQuery selector to select element with class 'time-block'
+  //iterates using the each() method
+  //toggleClass() method sets and updates the codition according to the time.
+  function hourlyColor() {
+    $('.time-block').each(function() {
+      const blockHour = parseInt(this.id);
+      $(this).toggleClass('past', blockHour < currentHour);
+      $(this).toggleClass('present', blockHour === currentHour);
+      $(this).toggleClass('future', blockHour > currentHour);
+    });
+  }
 
-  $('.saveBtn').on('click', function (){
-    console.log($(this).parent().attr('id'));
-    localStorage.setItem($(this).parent().attr('id'), $(this).siblings('textarea').val())
-  })
+  //the following function save user input into localStorage when save button in clicked.
+  function textEntry() {
+    $('.saveBtn').on('click', function() {
+      const key = $(this).parent().attr('id');
+      const value = $(this).siblings('.description').val();
+      localStorage.setItem(key, value);
+    })
+  }
 
-  // Added code to apply the past, present, or future class to each time
-  // for (var i = 9; i <= 17; i++) {
-  //   if (currentHour == i) {
-  //     $('#hour-' + i).attr("class", "row time-block present");
-  //   } else if (currentHour > i) {
-  //     $('#hour-' + i).attr("class", "row time-block past");
-  //   } else {
-  //     $('#hour-' + i).attr("class", "row time-block future");
-  //   }
-  // }
-  
-  // Added code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements
-  // $('#hour-9').children().eq(1).val(localStorage.getItem("hour-9"));
-  // $('#hour-10').children().eq(1).val(localStorage.getItem("hour-10"));
-  // $('#hour-11').children().eq(1).val(localStorage.getItem("hour-11"));
-  // $('#hour-12').children().eq(1).val(localStorage.getItem("hour-12"));
-  // $('#hour-13').children().eq(1).val(localStorage.getItem("hour-13"));
-  // $('#hour-14').children().eq(1).val(localStorage.getItem("hour-14"));
-  // $('#hour-15').children().eq(1).val(localStorage.getItem("hour-15"));
-  // $('#hour-16').children().eq(1).val(localStorage.getItem("hour-16"));
-  // $('#hour-17').children().eq(1).val(localStorage.getItem("hour-17"));
-  
+  //Refreshes the color to stick to the conditions of: past, present, future.
+  function refreshColor() {
+    $('.time-block').each(function() {
+      const blockHour = parseInt(this.id);
+      if (blockHour == currentHour) {
+        $(this).removeClass('past future').addClass('present');
+      } else if (blockHour < currentHour) {
+        $(this).removeClass('future present').addClass('past');
+      } else {
+        $(this).removeClass('past present').addClass('future');
+      }
+    });
+  }
 
-  $('#hour-10 textarea').text(localStorage.getItem("hour-10"))
-  // TODO: Add code to display the current date in the header of the page.
+  //retrieves user input from local storage
+$('.time-block').each(function() {
+  const key = $(this).attr('id');
+  const value = localStorage.getItem(key);
+  $(this).children('.description').val(value);
 });
 
+//displays the current time and date on the header of the page.
+function updateTime() {
+  const dateElement = $('#currentDay');
+  const timeElement = $('#currentTime');
+  const currentDate = dayjs().format('MMM D, dddd, YYYY');
+  const currentTime = dayjs().format('hh:mm:ss A');
+  dateElement.text(currentDate);
+  timeElement.text(currentTime);
+}
 
+//this calls all the functions and adds the live counter to the page header.
+hourlyColor();
+textEntry();
+refreshColor();
+setInterval(updateTime,1000);
+});
 
